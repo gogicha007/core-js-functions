61bc9879d47731cf01dd44ac9509d8fbb4e0643c
gogicha007 <gogicha@gmail.com>
2024-04-04 21:09:55 +0400

    five tasks done

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-function getPowerFunction(exponent) {
-  return (x) => x**exponent
+function getPolynom() {
+  if (!arguments) return null;
+  if(arguments.length === 1) return (x) => arguments[0]
+  if(arguments.length === 2) return (x) => arguments[0]*x+arguments[1]
+  return (x) => arguments[0]*(x**2)+arguments[1]*x+arguments[2];
 }
 
-console.log(getPowerFunctions(2))
+const poly = getPolynom(8)
+console.log(poly(5))
