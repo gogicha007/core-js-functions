9f36ab926d689967ecd99ab633c54d5e6e8ee3cf
gogicha007 <gogicha@gmail.com>
2024-04-05 16:06:11 +0400

    all tasks done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -223,8 +223,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startNo = startFrom - 1;
+  return () => {
+    startNo += 1;
+    return startNo;
+  };
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 function getIdGeneratorFunction(startFrom) {
-  throw new Error('Not implemented');
+  let startNo = startFrom;
+  return () => startNo++;
 }
 
-
-const getId4 = getIdGenerator(4);
-const getId10 = gerIdGenerator(10);
-console.log(getId4()) //=> 4
-console.log(getId10()) //=> 10
-console.log(getId4()) //=> 5
-console.log(getId4()) //=> 6
-console.log(getId4()) //=> 7
-console.log(getId10()) //=> 11
+const getId4 = getIdGeneratorFunction(4);
+const getId10 = getIdGeneratorFunction(10);
+console.log(getId4()); //=> 4
+console.log(getId10()); //=> 10
+console.log(getId4()); //=> 5
+console.log(getId4()); //=> 6
+console.log(getId4()); //=> 7
+console.log(getId10()); //=> 11
